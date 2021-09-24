2eb8a799a7410a7e9747f9ed23060215b7646776
lyfer233 <2217636939@qq.com>
2021-09-25 06:49:51 +0800

    Add files via upload

---
+++ 数字图像处理/zzj数字图像处理/fingerprint.m
@@ -0,0 +1,11 @@
+pic = imread('fingerprint.jpg');
+se = strel('square',3);
+pic_erode = imerode(pic,se);
+pic_open = imopen(pic,se);
+pic_dilate = imdilate(pic_open,se);
+pic_close = imclose(pic_dilate,se);
+figure,subplot(2,3,1);imshow(pic);title('‘≠Õº');
+subplot(2,3,2);imshow(pic_dilate);title('≈Ú’Õ');
+subplot(2,3,3);imshow(pic_erode);title('∏Ø ¥');
+subplot(2,3,4);imshow(pic_open);title('ø™');
+subplot(2,3,5);imshow(pic_close);title('πÿ');
